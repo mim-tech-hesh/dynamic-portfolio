bd402088fbe5775b04f762678d4600955a0696b2
mim-tech-hesh <mim15-5924@diu.edu.bd>
2025-05-28 13:02:41 +0600

    portfolio 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>HELLO,I'M SHAHADAT HUSSAIN UI/UX DESIGNER</title>
+    <style>
+        .
 </head>
 <body>
-    Hello laravel
+    <nav>
+        
+</nav>
+<section>
+<div class="disp-flex flex-justify-space">
+<div>
+<img src="{{asset('assets/images/img.png')}}"
+</div>
+<div>
+About
+</div>
+<div>
+<div class="disp-flex-flex"
+
+
 </body>
 </html>
